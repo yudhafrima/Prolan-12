96a87eb240e0b9b7d0a50abc620a2684f5bca4c4
Yudha Frima <yudhafrima@gmail.com>
2017-03-07 19:41:08 +0700

    Add files via upload

---
+++ minggu ke 3/kasir.java
@@ -0,0 +1,9 @@
+public class kasir {
+    protected String  nama,JenisKelamin,jenis;
+		Integer  umur;
+
+  protected void MelayaniTransaksi(){
+	System.out.println("Melayani transaksi");
+				}
+    
+}
